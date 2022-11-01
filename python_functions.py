2f4b75860d3db336f33ba185700426f49557d1d3
Tal <trimoni8@gmail.com>
2022-11-01 15:04:28 -0400

    exer 2

--- python_functions.py
+++ python_functions.py
@@ -1,5 +1,15 @@
 def sum_to(n):
   return sum(range(n+1))
 
+print(sum_to(6))
 print(sum_to(10))
 
+def largest(list):
+  max = list[0]
+  for x in list:
+    if x > max:
+      max = x
+  return max
+
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
